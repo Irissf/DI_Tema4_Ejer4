cfd8dc17b9aebb167f2272702547fc69f1374f00
Irissf <iris.seijo@gmail.com>
2020-12-04 20:29:05 +0100

    Arreglando fallos

--- DI_Tema4_Ejer4/Form1.cs
+++ DI_Tema4_Ejer4/Form1.cs
@@ -18,10 +18,10 @@ namespace DI_Tema4_Ejer4
         Delegado delOperacion;
         double num1 = 0;
         double num2 = 0;
-        bool divi = false;
         int seg = 0;
         int min = 0;
         Hashtable operacion = new Hashtable();
+        RadioButton rdb;
 
         public Form1()
         {
@@ -29,33 +29,49 @@ namespace DI_Tema4_Ejer4
             operacion.Add("sumar", new Delegado((num1, num2) => { return num1 + num2; }));
             operacion.Add("restar", new Delegado((num1, num2) => { return num1 - num2; }));
             operacion.Add("multiplicar", new Delegado((num1, num2) => { return num1 * num2; }));
-            operacion.Add("dividir", new Delegado((num1, num2) => { return num1 / num2; }));
+            operacion.Add("dividir", new Delegado((num1, num2) =>
+            {
+                if (num2 == 0)
+                {
+                    MessageBox.Show("Una división entre cero, da cero");
+                    return 0;
+                }
+                else
+                {
+                    return num1 / num2;
+                }
+            }));
 
             timer1.Enabled = true;
+
+            radioButton1.Tag = "+";
+            radioButton2.Tag = "-";
+            radioButton3.Tag = "*";
+            radioButton4.Tag = "/";
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try
+            if (textBox1.Text.Length > 0 && textBox2.Text.Length > 0)
             {
-                num1 = Convert.ToDouble(textBox1.Text);
-                num2 = Convert.ToDouble(textBox2.Text);
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("formato incorrecto");
-            }
-            if (divi && (num1 == 0 || num2 == 0))
-            {
-                label2.Text = "Divison entre 0 mal";
-                divi = false;
+                try
+                {
+                    num1 = Convert.ToDouble(textBox1.Text);
+                    num2 = Convert.ToDouble(textBox2.Text);
+                    label2.Text = String.Format("Resultado {0:0.###}", delOperacion(num1, num2))  ;
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("formato incorrecto");
+                }
+
             }
             else
             {
-                label2.Text = "Resultado " + delOperacion(num1, num2);
-
+                label2.Text = "Introduce 2 números";
             }
+
 
 
 
@@ -63,36 +79,14 @@ namespace DI_Tema4_Ejer4
 
         private void check(object sender, EventArgs e)
         {
-            RadioButton rdb = (RadioButton)sender;
-            if (rdb == radioButton1)
-            {
-                delOperacion = (Delegado)operacion[radioButton1.Text]; //asignamos el delegado de tipo Delegado al delegado delOperacion
-                label1.Text = "+";
+            rdb = (RadioButton)sender;
+            delOperacion = (Delegado)operacion[rdb.Text]; //asignamos el delegado de tipo Delegado al delegado delOperacion
+            label1.Text =Convert.ToString( rdb.Tag);
 
-            }
-            else if (rdb == radioButton2)
-            {
-                delOperacion = (Delegado)operacion[radioButton2.Text]; //asignamos el delegado de tipo Delegado al delegado delOperacion
-                label1.Text = "-";
-
-            }
-            else if (rdb == radioButton3)
-            {
-                delOperacion = (Delegado)operacion[radioButton3.Text]; //asignamos el delegado de tipo Delegado al delegado delOperacion
-                label1.Text = "*";
-
-            }
-            else if (rdb == radioButton4)
-            {
-                delOperacion = (Delegado)operacion[radioButton4.Text]; //asignamos el delegado de tipo Delegado al delegado delOperacion
-                label1.Text = "/";
-                divi = true;
-
-            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
-        { 
+        {
             //esto pasa cada 1000 milisegundos que es lo que le indicamos
             seg++;
             if (seg == 60)
